5bb7c94eb52793c52de409e7fd0b58d0ca9df7a2
visusvloid <91681617+visusvloid@users.noreply.github.com>
2021-10-03 18:15:09 +0700

    Bot

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$username =
+//masukkan username akun shopee kamu
+$linkproduk = "";
+$kurir=
+$variasi= 
+$stok =
+$kuantiti : ;
+$hargaflashsale = ;
+//sesuaikan dengan harga yang ditawarkan
+$metodepembayaran = ;
+//keik metode pembayaran yang kamu inginkan
+$waktuflashsale =
+  $jam =00;
+  $menit =00;
+  $detik =00;
+  //ketik sesuaikan
+  
+
